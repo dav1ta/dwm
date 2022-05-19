9633fa6afff901f70e934bb20f55e3e10f8acdd0
dav1ta <daviti.tchanturia.1@btu.edu.ge>
2022-05-19 13:09:01 +0400

    changes

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
@@ -11,18 +11,19 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const char *fonts[]          = { "Dejavu Sans Mono Nerd Font:size=13" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:style:medium:size=14",
+                                        "Material Design Icons-Regular:size=14" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#225577";
-static const char col_orange[]       = "#f99e03";
+static const char col_border[]        = "#fcd095";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_orange },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_border },
 };
 
 /* tagging */
@@ -40,7 +41,6 @@ static const Rule rules[] = {
 	{ "jetbrains-pycharm",    NULL,       NULL,           1 << 2,          0,           0,           -1 },
 	{ "Skype",                NULL,       NULL,           1 << 8,          0,           0,           -1 },
 	{ "subl",                 NULL,       NULL,           1 << 4,          0,           0,           -1 },
-	{ NULL,                   NULL,       "st_tag",       1 << 4,          1,           0,           -1 },
 	{ "mpv",                  NULL,       NULL,           0,               0,           1,           -1 },
 	{ NULL,                   NULL,       "st_float",     0,               0,           1,           -1 },
 };
@@ -81,7 +81,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st","-t","st_tag",NULL };
-static const char *termcmdfloat[]  = { "st", "-t","st_float","-g", "130x35", NULL };
+static const char *termcmdfloat[]  = { "st", "-t","st_float","-g", "100x30", NULL };
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "100x30", NULL };
 
 
 static Key keys[] = {
@@ -126,6 +128,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,     36,    spawn,          {.v = termcmd}},      //Enter
 	{ MODKEY,     			    40,    tagmon,         {.i = +1 } },      // period
   { MODKEY,               25,      winview,      {0} },
+  { MODKEY,               49,  togglescratch,  {.v = scratchpadcmd } },
 	TAGKEYS(                10,                    0)                 // 1
 	TAGKEYS(                11,                    1)                 // 2
 	TAGKEYS(                12,                    2)                 // 3
